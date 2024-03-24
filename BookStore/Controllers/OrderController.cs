cc35d124c1fc7021d903cc1bb102cae1aea8bbcd
Stoyan <terora66@gmail.com>
2024-03-24 12:12:43 +0200

    Created Controllers and corresponding views

---
+++ BookStore/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class OrderController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
