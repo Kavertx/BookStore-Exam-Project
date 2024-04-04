664567faf6b8cab40fe17212d5b00b6e2fafe2b1
Stoyan <terora66@gmail.com>
2024-04-04 18:00:05 +0300

    Some *temporary* changes and adjustments to the views

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Core.Contracts;
+using BookStore.Core.Models.Book;
+using BookStore.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
     public class BookController : BaseController
     {
-        public IActionResult Index()
+        private readonly IBookService bookService;
+        private readonly IClientService clientService;
+        public BookController(IBookService _bookService, IClientService _clientService)
         {
-            return View();
+            bookService = _bookService;
+            clientService = _clientService;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        { 
+            var model = await bookService.AllAsync();
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Favourites()
+        {
+            var model = await clientService.AllFavouriteBooksAsync(User.Id());
+            return View(model);
         }
     }
 }
