cadddee91618334ec52e16db7fa8652924527c01
Stoyan <terora66@gmail.com>
2024-04-12 14:46:09 +0300

    num

--- BookStore.Core/Services/OrderService.cs
+++ BookStore.Core/Services/OrderService.cs
@@ -35,7 +35,8 @@ namespace BookStore.Core.Services
             {
                 BuyerId = clientId,
                 TimeOfOrder = dateTime,
-                TotalPrice = totalPrice
+                TotalPrice = totalPrice,
+                NumberOfBooks = numberOfBooks
             });
             return await repository.SaveChangesAsync();
         }
